7449ac275e52246d40897440d62b9a695ddb3b4e
narendravs <narendravs_mca@yahoo.com>
2023-12-19 13:23:05 +0530

    Update index.tsx
    path for imgaes changed

--- src/scenes/ourClasses/index.tsx
+++ src/scenes/ourClasses/index.tsx
@@ -1,12 +1,12 @@
 import { SelectedPage, ClassType } from "../../shared/types";
 import { motion } from "framer-motion";
 import HText from "../../shared/HText";
-import image1 from "@/assets/image1.png";
-import image2 from "@/assets/image2.png";
-import image3 from "@/assets/image3.png";
-import image4 from "@/assets/image4.png";
-import image5 from "@/assets/image5.png";
-import image6 from "@/assets/image6.png";
+import image1 from "../../assets/image1.png";
+import image2 from "../../assets/image2.png";
+import image3 from "../../assets/image3.png";
+import image4 from "../../assets/image4.png";
+import image5 from "../../assets/image5.png";
+import image6 from "../../assets/image6.png";
 import Class from "./Class";
 
 const classes: Array<ClassType> = [
